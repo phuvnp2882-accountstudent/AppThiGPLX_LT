df0c80e02b231f99c030f643863eafc415859b11
Nguyen Pham Thanh Duy <thanhduy@example.com>
2025-11-09 00:58:28 +0700

    code OnLyThuyet.kt

--- app/src/main/java/com/example/appthigplx_lt/OnLyThuyet.kt
+++ app/src/main/java/com/example/appthigplx_lt/OnLyThuyet.kt
@@ -1,2 +1,33 @@
 package com.example.appthigplx_lt
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OnLyThuyet(navController: NavController, chuDe: String) {
+    val context = LocalContext.current
+    val db = remember { MyDbHelper(context) }
+
+    var listLyThuyet by remember { mutableStateOf(listOf<LyThuyet>()) }
+    var currentIndex by remember { mutableStateOf(0) }
+    var selectedAnswer by remember { mutableStateOf<String?>(null) }
+    var showResult by remember { mutableStateOf(false) }
+    var tongCau by remember { mutableStateOf(0) }
+    var soCauDung by remember { mutableStateOf(0) }
